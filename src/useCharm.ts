63b67949dbd4dcf8e540fc6f8bb2d283c2665d6c
Dmitry Kaigorodov <dima.kaigorodov@gmail.com>
2024-05-29 13:33:50 +0200

    biome format

--- src/useCharm.ts
+++ src/useCharm.ts
@@ -2,18 +2,18 @@ import { useEffect, useState } from "react";
 import type { Charm, Subscriber } from "./charm";
 
 export const useCharm = <Value>(charm: Charm<Value>) => {
-  const [value, setValue] = useState(charm.get());
+	const [value, setValue] = useState(charm.get());
 
-  useEffect(() => {
-    const subscriber: Subscriber<Value> = (
-      nextValue: Value,
-      prevValue: Value,
-    ) => {
-      setValue(nextValue);
-    };
-    charm.sub(subscriber);
-    return () => charm.unsub(subscriber);
-  });
+	useEffect(() => {
+		const subscriber: Subscriber<Value> = (
+			nextValue: Value,
+			prevValue: Value,
+		) => {
+			setValue(nextValue);
+		};
+		charm.sub(subscriber);
+		return () => charm.unsub(subscriber);
+	});
 
-  return [value, setValue];
+	return [value, setValue];
 };
--- src/utils.ts
+++ src/utils.ts
@@ -1,94 +1,93 @@
 import { charm, type Charm, type CharmConfig, type UpdateFn } from "./charm";
 
 export const charmSetter = <Value>(charm: Charm<Value>) => {
-  return (value: Value) => charm.set(value);
+	return (value: Value) => charm.set(value);
 };
 export const charmUpdater = <Value>(charm: Charm<Value>) => {
-  return (updateFn: UpdateFn<Value>) => charm.update(updateFn);
+	return (updateFn: UpdateFn<Value>) => charm.update(updateFn);
 };
 export const charmGetter = <Value>(charm: Charm<Value>) => {
-  return () => charm.get();
+	return () => charm.get();
 };
 
-
 export type GetByPathFn<BaseValue, DerivedValue> = (
-  value: BaseValue,
+	value: BaseValue,
 ) => DerivedValue;
 export type SetByPathFn<BaseValue, DerivedValue> = (
-  nextDerivedValue: DerivedValue,
-  prevBaseValue: BaseValue,
+	nextDerivedValue: DerivedValue,
+	prevBaseValue: BaseValue,
 ) => BaseValue;
 
 export const inheritCharm = <BaseValue, DerivedValue>(
-  base: Charm<BaseValue>,
-  getByPath: GetByPathFn<BaseValue, DerivedValue>,
-  setByPath: SetByPathFn<BaseValue, DerivedValue>,
-  charmConfig?: CharmConfig<DerivedValue>,
+	base: Charm<BaseValue>,
+	getByPath: GetByPathFn<BaseValue, DerivedValue>,
+	setByPath: SetByPathFn<BaseValue, DerivedValue>,
+	charmConfig?: CharmConfig<DerivedValue>,
 ) => {
-  const superCharm = charm(getByPath(base.get()));
-  return {
-    ...superCharm,
-    get() {
-      return getByPath(base.get());
-    },
-    set(nextValue: DerivedValue) {
-      superCharm.set(nextValue);
-      const nextBaseValue = setByPath(nextValue, base.get());
-      base.set(nextBaseValue);
-    },
-  };
+	const superCharm = charm(getByPath(base.get()));
+	return {
+		...superCharm,
+		get() {
+			return getByPath(base.get());
+		},
+		set(nextValue: DerivedValue) {
+			superCharm.set(nextValue);
+			const nextBaseValue = setByPath(nextValue, base.get());
+			base.set(nextBaseValue);
+		},
+	};
 };
 
 export const propertyCharm = <Value, PropValue>(
-  base: Charm<Value>,
-  propName: string,
-  config?: CharmConfig<Value>,
+	base: Charm<Value>,
+	propName: string,
+	config?: CharmConfig<Value>,
 ) => {
-  return inheritCharm(
-    base,
-    (base) => base[propName],
-    (nextPropValue, base) => ({ ...base, [propName]: nextPropValue }),
-    config,
-  );
+	return inheritCharm(
+		base,
+		(base) => base[propName],
+		(nextPropValue, base) => ({ ...base, [propName]: nextPropValue }),
+		config,
+	);
 };
 
 export const arrayEltCharm = <Value>(
-  base: Charm<Array<Value>>,
-  index: number,
-  config?: CharmConfig<Value>,
+	base: Charm<Array<Value>>,
+	index: number,
+	config?: CharmConfig<Value>,
 ) => {
-  return inheritCharm<Array<Value>, Value>(
-    base,
-    (value: Array<Value>) => value[index],
-    (nextEltValue, base) => {
-      const nextBase = [...base];
-      nextBase[index] = nextEltValue;
-      return nextBase;
-    },
-    config,
-  );
+	return inheritCharm<Array<Value>, Value>(
+		base,
+		(value: Array<Value>) => value[index],
+		(nextEltValue, base) => {
+			const nextBase = [...base];
+			nextBase[index] = nextEltValue;
+			return nextBase;
+		},
+		config,
+	);
 };
 
 export const debouncedCharm = <Value>(
-  baseCharm: Charm<Value>,
-  delay: number,
+	baseCharm: Charm<Value>,
+	delay: number,
 ) => {
-  let timeoutId: ReturnType<typeof setTimeout> | undefined = undefined;
-  let latestValue: Value = baseCharm.get();
-  const superCharm = charm(baseCharm.get());
+	let timeoutId: ReturnType<typeof setTimeout> | undefined = undefined;
+	let latestValue: Value = baseCharm.get();
+	const superCharm = charm(baseCharm.get());
 
-  return {
-    ...superCharm,
-    set(nextValue: Value) {
-      if (timeoutId !== undefined) {
-        clearTimeout(timeoutId);
-        timeoutId = undefined;
-      }
-      timeoutId = setTimeout(() => {
-        superCharm.set(nextValue);
-        latestValue = nextValue;
-        timeoutId = undefined;
-      }, delay);
-    },
-  };
+	return {
+		...superCharm,
+		set(nextValue: Value) {
+			if (timeoutId !== undefined) {
+				clearTimeout(timeoutId);
+				timeoutId = undefined;
+			}
+			timeoutId = setTimeout(() => {
+				superCharm.set(nextValue);
+				latestValue = nextValue;
+				timeoutId = undefined;
+			}, delay);
+		},
+	};
 };
